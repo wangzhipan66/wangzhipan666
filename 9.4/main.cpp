2a183318a444cd47e0b24c214b66594ba8fa1fc9
Wangzhipan <1736986309@qq.com>
2019-05-12 19:33:57 +0800

    no message

---
+++ 9.4/main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+#include "Time.h"
+
+using namespace std;
+
+int main()
+{
+   Time t;
+
+
+   cout << "The universal time is ";
+
+   t.printUniversal();
+
+   cout << "\nThe standard time is ";
+
+   t.printStandard();
+
+   cout << endl;
+}
