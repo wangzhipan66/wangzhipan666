02ea84191cd872c2d7c4f475e5f9272535b882f9
Wangzhipan <1736986309@qq.com>
2019-06-25 00:04:36 +0800

    no message

---
+++ 7.11/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int counts[10]={0};
+    for(int i=0;i<15;i++)
+        bonus[i]+=1;
+    for(int i=0;i<12;i++)
+        cin>>monthlyTemperatures[i];
+    for(int i=0;i<5;i++)
+        cout<<bestScores[i]<<endl;
+    return 0;
+}
