b1df3b8420949bef38fe563f86403ea36f6f89a4
Wangzhipan <1736986309@qq.com>
2019-06-24 23:51:13 +0800

    no message

--- 5.19/main.cpp
+++ 5.19/main.cpp
@@ -1,12 +1,22 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-double pai(double n)
-{    double p=0;
-for(int i=1;i<=n;i++)
-{        if(i%2!=0)            p+=4/(2*(double)i-1);
-else            p-=4/(2*(double)i-1);    }
-return p;}
+double pai(int n)
+{
+    double p=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+            p-=4/(2*(double)i-1);
+        else
+            p+=4/(2*(double)i-1);
+    }
+    return p;
+}
 int main()
-{    for(double i=1;i<=1000;i++)
-{        cout<<"第"<<i<<"项近似值为: "<<pai(i)<<endl;    }
-return 0;}
+{
+    for(int n=1;n<=1000;n++)
+    {
+        cout<<n<<":"<<pai(n)<<endl;
+    }
+    return 0;
+}
