20e7f0456a09426d89de3e640a26fbb0a47cc488
Wangzhipan <1736986309@qq.com>
2019-06-25 00:25:13 +0800

    no message

---
+++ 8.11/main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<*number<<endl;
+
+    *integerPtr=*realPtr;
+
+    x=&y;
+
+    char *s="this is a character array";
+
+    double xPtr=x;
+
+    return 0;
+}
